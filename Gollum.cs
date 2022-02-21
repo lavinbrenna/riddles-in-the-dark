6c4b5e257ebb654fb7138652635a3bce59fab7ee
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-21 14:44:34 -0800

    update files
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- Gollum.cs
+++ Gollum.cs
@@ -9,21 +9,21 @@ namespace cave
     {
       Console.WriteLine("Answer Gollum's riddles correctly or Gollum will bash you over the head and eat you like a tasty snack");
       while(Gollum.Questions.Count > 0){
-          int randomNumber = Gollum.GetRandomNumber();
-          string riddle = Gollum.GetRiddle(randomNumber);
-          string answer = Gollum.GetAnswer(randomNumber);
-          Console.WriteLine(riddle);
-          Console.WriteLine("What is the answer?");
-          string userAnswer = Console.ReadLine();
-          if(userAnswer.ToLower().Contains(answer)){
-            Console.WriteLine("You answered the riddle correctly!");
-            Gollum.RemoveRiddle(randomNumber);
-          }
-          else{
-          Console.WriteLine("You have been bashed over the head and eaten by Gollum.");
-          break;
-          }
+        int randomNumber = Gollum.GetRandomNumber();
+        string riddle = Gollum.GetRiddle(randomNumber);
+        string answer = Gollum.GetAnswer(randomNumber);
+        Console.WriteLine(riddle);
+        Console.WriteLine("What is the answer?");
+        string userAnswer = Console.ReadLine();
+        if(userAnswer.ToLower().Contains(answer)){
+          Console.WriteLine("You answered the riddle correctly!");
+          Gollum.RemoveRiddle(randomNumber);
         }
+        else{
+        Console.WriteLine("You have been bashed over the head and eaten by Gollum.");
+        break;
+        }
+      }
     }
   }
 }
